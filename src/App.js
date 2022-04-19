d3556159b65362415bb1a85e441523f89a23061d
ldhldh <ldhldh@inforang.com>
2022-04-19 17:32:34 +0900

    add props

--- src/App.js
+++ src/App.js
@@ -3,9 +3,28 @@ import Table from './Table';
 
 class App extends Component {
   render() {
+    const characters = [
+      {
+        name: 'BoA',
+        job: 'Dancer',
+      },
+      {
+        name: 'Youra',
+        job: 'Designer',
+      },
+      {
+        name: 'DH',
+        job: 'Developer',
+      },
+      {
+        name: 'Yowon',
+        job: 'Actor',
+      },
+    ]
+    
     return (
       <div className='container'>
-        <Table />
+        <Table characterData={characters} />
       </div>
     )
   }
--- src/Table.js
+++ src/Table.js
@@ -15,18 +15,29 @@ const TableHeader = () => {
 }
 
 // Simple Component 2 // 한 줄짜리는 return의 () 생략 가능.
-const TableBody = () => {
-  return <tbody />
+const TableBody = (props) => {
+  const rows = props.characterData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+      </tr>
+    )
+  })
+
+  return <tbody>{rows}</tbody>
 }
 
 // Class Component 
 // 클래스 컴포넌트에는 render() 가 포함되어야하며 return 은 하나의 상위 요소만 반환할 수 있다.
 class Table extends Component {
   render() {
+    const {characterData} = this.props
+
     return (
     <table>
       <TableHeader />
-      <TableBody />
+      <TableBody characterData={characterData} />
     </table>
     )
   }
